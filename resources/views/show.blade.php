44c9b378d0dc00a983e45cf5220a57ceb1ae95ed
Lucas Souza de Andrade <lucascorporation@live.com>
2022-04-27 20:04:20 -0300

    Set Status Dialog

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -58,20 +58,50 @@
     <div class="buttons-container flex items-center justify-between mt-6">
         
         <div class="flex items-center space-x-4 ml-6">
+        <div class=" relative">
             <button type="button" class="flex items-center justify-center h-11 w-32 text-xs bg-blue
             font-semibold rounded-xl border border-blue hover:bg-blue-hover
             transition duration-150 ease-in px-6 py-3 text-white">
             <span class="ml-1">Reply</span>
             </button>
+            <div class="absolute z-10 w-104 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
+                <form action="#" class="space-y-4 px-4 py-6">
+                    <div >
+                        <textarea name="post_comment" id="post_comment" cols="30" rows="4"
+                        class="w-full text-sm bg-gray-100 roudnded-xl placeholder-gray-900
+                        border-none px-4 py-2" placeholder="Go ahead, don't be shy . Share your thoughts ...."></textarea>
+                    </div>
+                    <div class="flex items-center space-x-3">
+                        <button type="button" class="flex items-center justify-center h-11 w-1/2 text-xs bg-blue
+                        font-semibold rounded-xl border border-blue hover:bg-blue-hover
+                        transition duration-150 ease-in px-6 py-3 text-white">
+                        Post Comment
+                        </button>
 
-            <button type="button" class="flex items-center justify-center h-11 w-36 text-xs bg-gray-200
-            font-semibold rounded-xl border border-gray-200 hover:border-gray-400
-            transition duration-150 ease-in px-6 py-3">
-                <span>Set Status</span>
-                <svg  class="h-4 w-4 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-                  </svg>
-            </button>
+                        <button type="button"
+                        class="flex items-center justify-center w-32 h-11 text-xs bg-gray-200
+                                font-semibold rounded-xl border border-gray-200 hover:border-gray-400
+                                transition duration-150 ease-in px-6 py-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-500 h-4 w-4 -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                                  </svg>
+                        <span class="ml-1">Attach</span>
+                    </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+       <div class="relative">
+        <button type="button" class="flex items-center justify-center h-11 w-36 text-sm bg-gray-200
+        font-semibold rounded-xl border border-gray-200 hover:border-gray-400
+        transition duration-150 ease-in px-6 py-3">
+            <span>Set Status</span>
+            <svg  class="h-4 w-4 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+              </svg>
+        </button>
+       </div>
         </div>
 
         <div class="flex items-center space-x-3">
@@ -111,7 +141,7 @@
                             <div class="font-bold text-gray-900">Mary Doe</div>
                             <div>&bull;</div>
                             <div>10 hours ago</div>
-                            <div>&bull;</div>
+                            
                     
                         </div>
                         <div class="flex items-center space-x-2">
@@ -153,7 +183,7 @@
                             <div class="font-bold text-blue">Andrea</div>
                             <div>&bull;</div>
                             <div>10 hours ago</div>
-                            <div>&bull;</div>
+                            
                     
                         </div>
                         <div class="flex items-center space-x-2">
@@ -195,7 +225,7 @@
                                 <div class="font-bold text-gray-900">Mark Doe</div>
                                 <div>&bull;</div>
                                 <div>10 hours ago</div>
-                                <div>&bull;</div>
+                                
                         
                             </div>
                             <div class="flex items-center space-x-2">
